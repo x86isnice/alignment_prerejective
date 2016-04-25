0d20c5d1d7bdc9789380239899934980ce9b28e7
x86isnice <x86isnice@gmail.com>
2016-04-25 15:56:30 +0800

    changes log

---
+++ source/alignment_prerejective.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+
+int main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);
+
+  pcl::io::loadPCDFile("2.pcd",*cloud_in); 
+  pcl::io::loadPCDFile("1.pcd",*cloud_out);
+
+  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+  icp.setInputCloud(cloud_in);
+  icp.setInputTarget(cloud_out);
+  pcl::PointCloud<pcl::PointXYZ> Final;
+  icp.align(Final);
+ 
+  std::cout << "has converged:" << icp.hasConverged() << " score: " <<
+  icp.getFitnessScore() << std::endl;
+  std::cout << icp.getFinalTransformation() << std::endl;
+  pcl::io::savePCDFileASCII("output.pcd",Final);
+  
+  return (0);
+}
